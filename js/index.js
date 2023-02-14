145f6d2da5c5c3e2700885711bfc8b664b7256ef
Augustin Anders <augustinanders@gmail.com>
2023-02-14 12:05:32 +0100

    my_solution

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]');
+
+const lightModeButton = document.querySelector('[data-js="light-mode-button"]');
+
+const toggleModeButton = document.querySelector('[data-js="toggle-button"]');
+
+darkModeButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+lightModeButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+toggleModeButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
